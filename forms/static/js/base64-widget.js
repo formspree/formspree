4d6d3f2c81c504b4e500374a7b843a34864f236b
fiatjaf@spooner <fiatjaf@gmail.com>
2014-12-29 19:55:48 -0500

    change all the example snippets with the b64-encoded address
    see https://assembly.com/formspree/bounties/8#ad5dfd82-da2f-4564-a4d7-d9782e4bc302

--- forms/static/js/base64-widget.js
+++ forms/static/js/base64-widget.js
@@ -1,11 +1,20 @@
 $(function () {
-  $('.b64-input').on('input', function () { updateWith($(this)) })
+  $('.b64-input').on('input', function () {
+    updateWith($(this), true)
+  })
 
-  function updateWith ($input) {
-    var decoded = ($input.val().trim() || 'you@example.com')
+  function updateWith ($input, force) {
+    var decoded = ($input.val().trim() || $input.text().trim() || 'you@email.com')
     var encodedURL = $input.data('baseaddr') + btoa(decoded)
     $('.b64-mirror').text(decoded)
-    $('.b64-output-url').text(encodedURL)
+    $('.b64-output-url').each(function () {
+      $output = $(this)
+      var preexisting = $output.val().trim() || $output.text().trim()
+      if (preexisting && !force) {
+        return
+      }
+      $output.text(encodedURL).val(encodedURL)
+    })
   }
 
   updateWith($('.b64-input'))
