216d9adff8c1bc493ebfd71cdce51aada19f4b44
fiatjaf@spooner <fiatjaf@gmail.com>
2015-01-05 07:59:26 -0500

    replace redis with postgres using sqlalchemy

--- forms/settings.py
+++ forms/settings.py
@@ -3,8 +3,12 @@ import os
 # load a bunch of environment
 
 DEBUG = os.getenv('DEBUG') in ['True', 'true', '1', 'yes']
+if DEBUG:
+    SQLALCHEMY_ECHO = True
 
 NONCE_SECRET = os.getenv('NONCE_SECRET')
+SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+
 REDIS_URL = os.getenv('REDISTOGO_URL') or os.getenv('REDISGREEN_URL') or 'http://localhost:6379'
 
 SERVICE_NAME = os.getenv('SERVICE_NAME') or 'Forms'
