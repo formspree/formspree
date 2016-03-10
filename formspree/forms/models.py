e06e872dd9d6ec7bb92b3643493e27a4aa24f6b1
Rohit Datta <rohitdatta11@gmail.com>
2016-03-10 01:40:46 -0600

    Removed inclusion of email format stuff

--- formspree/forms/models.py
+++ formspree/forms/models.py
@@ -106,7 +106,6 @@ class Form(DB.Model):
         cc = data.get('_cc', None)
         next = next_url(referrer, data.get('_next'))
         spam = data.get('_gotcha', None)
-        format = data.get('_format', None)
 
         # prevent submitting empty form
         if not any(data.values()):
@@ -155,11 +154,7 @@ class Form(DB.Model):
         now = datetime.datetime.utcnow().strftime('%I:%M %p UTC - %d %B %Y')
         if not overlimit:
             text = render_template('email/form.txt', data=data, host=self.host, keys=keys, now=now)
-            # check if the user wants a new or old version of the email
-            if format == 'text':
-                html = render_template('email/text_form.html', data=data, host=self.host, keys=keys, now=now)
-            else:
-                html = render_template('email/form.html', data=data, host=self.host, keys=keys, now=now)
+            html = render_template('email/form.html', data=data, host=self.host, keys=keys, now=now)
         else:
             if monthly_counter - settings.MONTHLY_SUBMISSIONS_LIMIT > 25:
                 # only send this overlimit notification for the first 25 overlimit emails
