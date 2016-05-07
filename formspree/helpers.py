b2d15aa3ac5bf660877c84674865658eec8fe5ee
Rohit Datta <rohitdatta11@gmail.com>
2016-05-07 15:20:30 -0500

    look at x-forwarded-proto headers too

--- formspree/helpers.py
+++ formspree/helpers.py
@@ -5,7 +5,7 @@ def ssl_redirect(app):
     app.after_request(set_headers)
     
 def get_redirect():
-    if not request.is_secure and request.method == 'GET' and request.url.startswith('http://'):
+    if not request.is_secure and not request.headers.get('X-Forwarded-Proto', 'http') == 'https' and request.method == 'GET' and request.url.startswith('http://'):
         url = request.url.replace('http://', 'https://', 1)
         r = redirect(url, code=301)
         return r
