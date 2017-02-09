dcf97f4517e2c6eb32658c97fbce3c7f625888f3
Rohit Datta <rohitdatta11@gmail.com>
2017-02-09 14:24:16 +0100

    allow backend option to disable captcha

--- formspree/forms/models.py
+++ formspree/forms/models.py
@@ -25,6 +25,7 @@ class Form(DB.Model):
     confirmed = DB.Column(DB.Boolean)
     counter = DB.Column(DB.Integer)
     owner_id = DB.Column(DB.Integer, DB.ForeignKey('users.id'))
+    disable_captcha = DB.Column(DB.Boolean)
 
     owner = DB.relationship('User') # direct owner, defined by 'owner_id'
                                     # this property is basically useless. use .controllers
