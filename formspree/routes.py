7b742e3726cb81f073ebd096f899880358a59311
Rohit Datta <rohitdatta11@gmail.com>
2017-05-16 12:02:13 +0200

    alter order to define Stripe

--- formspree/routes.py
+++ formspree/routes.py
@@ -16,7 +16,7 @@ def configure_routes(app):
 
     # Users
     app.add_url_rule('/account', 'account', view_func=users.views.account, methods=['GET'])
-    app.add_url_rule('/account/upgrade', view_func=users.views.upgrade, methods=['POST'])
+    app.add_url_rule('/account/upgrade', 'account-upgrade', view_func=users.views.upgrade, methods=['POST'])
     app.add_url_rule('/account/downgrade', view_func=users.views.downgrade, methods=['POST'])
     app.add_url_rule('/account/resubscribe', view_func=users.views.resubscribe, methods=['POST'])
     app.add_url_rule('/card/add', 'add-card', view_func=users.views.add_card, methods=['POST'])
