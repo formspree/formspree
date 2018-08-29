4d332bdacb4efa0b3a7c660e0240f4dd06d1fc94
fiatjaf <fiatjaf@gmail.com>
2018-08-29 01:20:48 +0000

    fix create-form notice for non-upgraded users.

--- formspree/js/forms/FormList.js
+++ formspree/js/forms/FormList.js
@@ -99,7 +99,7 @@ module.exports = class FormList extends React.Component {
 
           {this.state.enabled_forms.length === 0 &&
           this.state.disabled_forms.length === 0 &&
-          this.user.upgraded ? (
+          this.state.user.upgraded ? (
             <h6 className="light">
               You don't have any forms associated with this account, maybe you
               should <a href="/account">verify your email</a>.
