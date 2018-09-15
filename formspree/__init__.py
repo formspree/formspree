cf58b3099d373f45c677ecafa530bfdf4b348787
Cole Krumbholz <cole@colevscode.io>
2018-09-14 22:20:47 -0400

    adding some comments

--- formspree/__init__.py
+++ formspree/__init__.py
@@ -4,6 +4,11 @@ from .create_app import create_app
 app = create_app()
 
 def debuggable_app():
+    """ For launching with gunicorn from a Heroku Procfile. 
+    Problem: both the web and worker processes run the same create_app code. If we start a ptvsd service in create_app, it will be
+    started twice on the same port, and fail. 
+    Solution: gunicorn gets its app object through this method that also starts the debug server. 
+    """
     if settings.DEBUG:
         import ptvsd
         ptvsd.enable_attach(address=('0.0.0.0', 3000))
